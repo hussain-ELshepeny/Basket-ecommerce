e620bb912a57e659c52eb73b89a6fc2a6f6db2ee
Abdelrahmanmahjob <114360935+Abdelrahmanmahjob@users.noreply.github.com>
2025-11-04 15:18:40 +0200

    Add banner-box section to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import BannerBox from "../components/home-components/BannerBox";
 import Offers from "../components/home-components/Offers";
 
 export default function Home() {
   return (
     <main>
       <Offers />
+      <BannerBox />
     </main>
   );
 }
