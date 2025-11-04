f6ea4a8196396b657c85f372878cd89f21708ba8
hussain-ELshepeny <hussainacc1@gmail.com>
2025-11-04 17:41:12 +0200

    Editing Section-Heading Component

--- src/components/home-components/BestSellers.jsx
+++ src/components/home-components/BestSellers.jsx
@@ -1,18 +1,15 @@
 import ProductsSlider from "./ProductSlider";
-import SectionHeading from "../shared/SectionHeading"
+import SectionHeading from "../shared/SectionHeading";
 import { MdOutlineArrowRightAlt } from "react-icons/md";
 
 export default function BestSellers() {
   return (
     <div className="section-container my-[var(--section-margin)]">
-   <div className="flex justify-between items-center">
-       <SectionHeading
-        headingText={"Best Sellers"}
-        spanText={"Do not miss the current offers until the end of March."}
-      />
-    <button className="flex gap-2 items-center cursor-pointer rounded-[30px] px-5 py-1 border border-[#D9D9E9] text-[#9B9BB4]">View All <MdOutlineArrowRightAlt /> </button>
-   </div>
-      <ProductsSlider />
+        <SectionHeading
+          headingText={"Best Sellers"}
+          spanText={"Do not miss the current offers until the end of March."}
+        />
+        <ProductsSlider />
     </div>
   );
 }
--- src/components/shared/SectionHeading.jsx
+++ src/components/shared/SectionHeading.jsx
@@ -1,5 +1,8 @@
+import { MdOutlineArrowRightAlt } from "react-icons/md";
+
 export default function SectionHeading({ headingText, spanText }) {
   return (
+    <div className="flex justify-between items-center">
     <div className="mb-[var(--section-margin)]">
       <h2 className="text-heading uppercase font-body font-semibold leading-[24px] text-[20px] tracking-[0px]">
         {/* semibold == 600 */}
@@ -8,6 +11,10 @@ export default function SectionHeading({ headingText, spanText }) {
       <span className="text-subHeading font-subHeading font-[400] leading-[18px] text-[12px] tracking-[-0.1px]">
         {spanText}
       </span>
+    </div>
+      <button className="flex gap-2 items-center cursor-pointer rounded-[30px] px-5 py-1 border border-[#D9D9E9] text-[#9B9BB4]">
+          View All <MdOutlineArrowRightAlt />
+        </button>
     </div>
   );
 }
