5a300df4657e6cd0e66d74c260c0664fe36fbbb6
hussain-ELshepeny <hussainacc1@gmail.com>
2025-11-05 13:16:03 +0200

    Add Dsicount Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import Offers from "../components/home-components/Offers";
 import BannerBox from "../components/home-components/BannerBox";
 import BestSellers from "../components/home-components/BestSellers";
-
+import Discount from "../components/home-components/Discount";
 export default function Home() {
   return (
     <main>
       <Offers />
       <BestSellers />
       <BannerBox />
+      <Discount />
     </main>
   );
 }
