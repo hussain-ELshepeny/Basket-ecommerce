ff84c11130e566bd21bab6ea4e322a85b5e83f95
hussain-ELshepeny <hussainacc1@gmail.com>
2025-11-15 21:54:17 +0200

    Edit Discount.jsx for Deployment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Offers from "../components/home-components/Offers"
 import BannerBox from "../components/home-components/BannerBox"
 import BestSellers from "../components/home-components/BestSellers"
 import Category from "../components/home-components/Category"
-import Discount from "../components/home-components/discount"
+import Discount from "../components/home-components/Discount"
 
 import Deals from "../components/home-components/Deals"
 import CategoryCollections from "../components/home-components/CategoryCollections"
